eee65233275c1beef442289de86581c641524737
KenjiMaehara <kmaehara@mel.co.jp>
2018-02-21 17:20:56 +0900

    testtest22
    testtest22

--- bossanPost.py
+++ bossanPost.py
@@ -2,12 +2,12 @@
 
 from slacker import Slacker
 import slackbot_settings
-from plugins import *
+from plugins import my_mention
 
 if __name__ == '__main__':
     slack = Slacker(slackbot_settings.API_TOKEN)
     slack.chat.post_message('general','こんにちわー')
-    if securityMode == 0:
+    if my_mention.securityMode == 0:
         slack.chat.post_message('general','現在、防犯解除中です')
     else:
         slack.chat.post_message('general','現在、防犯開始中です')
