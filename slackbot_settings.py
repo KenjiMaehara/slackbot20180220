80ce6715efbd86d0ec8a4150d7b06a7aff1f8dc5
KenjiMaehara <kmaehara@mel.co.jp>
2018-02-21 11:37:28 +0900

    testtest005
    testtest005

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -1,10 +1,10 @@
 # coding: utf-8
 
 # botアカウントのトークンを指定
-API_TOKEN = "xoxb-317171637541-qwMh1rMDqPJaSPUbdukK08k9"
+API_TOKEN = "*******************************"
 
 # このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
 DEFAULT_REPLY = "何言ってんだこいつ"
 
 # プラグインスクリプトを置いてあるサブディレクトリ名のリスト
-PLUGINGS = ['plugins']
+PLUGINS = ['plugins']
