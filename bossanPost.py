964b912de653b86e565b7d8603ec7a73f7c1d833
KenjiMaehara <kmaehara@mel.co.jp>
2018-02-21 17:01:12 +0900

    testtest18
    testtest18

--- bossanPost.py
+++ bossanPost.py
@@ -2,7 +2,7 @@
 
 from slacker import Slacker
 import slackbot_settings
-from .plugins.my_mention import *
+from .. import *
 
 if __name__ == '__main__':
     slack = Slacker(slackbot_settings.API_TOKEN)
