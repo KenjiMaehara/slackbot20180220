2feb48f8f9edd66191500c456775c5c52f087af9
KenjiMaehara <kmaehara@mel.co.jp>
2018-02-23 14:59:03 +0900

    testtest49
    testtest49

--- plugins/my_mention.py
+++ plugins/my_mention.py
@@ -104,6 +104,24 @@ def hello():
     else:
         timeCountSecurityOn = -1
 
+
+    buttonPin = 4
+    # GPIO初期化
+    wiringpi.wiringPiSetupGpio()
+    # GPIOを出力モード（1）に設定
+    wiringpi.pinMode( button_pin, 0 )
+    # 端子に何も接続されていない場合の状態を設定
+    # 3.3Vの場合には「2」（プルアップ）
+    # 0Vの場合は「1」と設定する（プルダウン）
+    wiringpi.pullUpDnControl( button_pin, 2 )
+
+    if( wiringpi.digitalRead(button_pin) == 0 ):
+        print ("Switch ON")
+    else:
+        print ("Switch OFF")
+
+
+
     print("現在のスレッドの数: " + str(threading.activeCount()))
     print("[%s] helohelo!!" % threading.currentThread().getName())
     t=threading.Timer(1,hello)
